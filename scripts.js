4ba470bba8bbfb8f514175781481be0be597a7c6
Aditya Bose <13054902+adbose@users.noreply.github.com>
2020-09-07 16:06:39 +0530

    Update scripts for deposit and withdraw

--- scripts.js
+++ scripts.js
@@ -1,24 +1,26 @@
+// TODO: Reload balance on response of transaction completion
+
 var contract;
 
 $(document).ready(function(){
     let web3 = new Web3(Web3.givenProvider);
 
     // Smart contract address deployed in the Ropsten Testnet
-    var address = "0x309218DbD0e2DA8604df43bE0C83F1719F9D3623";
+    var address = "0x86C05047E8b79c327703866559b06aDf0Ef3b733";
     // Smart contract ABI
     var abi = [
         {
-            "constant": false,
-            "inputs": [
+            "constant": true,
+            "inputs": [],
+            "name": "getBalance",
+            "outputs": [
                 {
-                    "name": "amt",
+                    "name": "",
                     "type": "int256"
                 }
             ],
-            "name": "deposit",
-            "outputs": [],
             "payable": false,
-            "stateMutability": "nonpayable",
+            "stateMutability": "view",
             "type": "function"
         },
         {
@@ -36,24 +38,24 @@ $(document).ready(function(){
             "type": "function"
         },
         {
+            "constant": false,
+            "inputs": [
+                {
+                    "name": "amt",
+                    "type": "int256"
+                }
+            ],
+            "name": "deposit",
+            "outputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
             "inputs": [],
             "payable": false,
             "stateMutability": "nonpayable",
             "type": "constructor"
-        },
-        {
-            "constant": true,
-            "inputs": [],
-            "name": "getBalance",
-            "outputs": [
-                {
-                    "name": "",
-                    "type": "int256"
-                }
-            ],
-            "payable": false,
-            "stateMutability": "view",
-            "type": "function"
         }
     ]
 
@@ -61,5 +63,37 @@ $(document).ready(function(){
 
     contract.methods.getBalance().call().then(function(bal) {
         $('#balance').append(bal);
+    })
+
+    $('#deposit').click(function() {
+        var amt = 0;
+        amt = parseInt($('#amount').val());
+
+        web3.eth.getAccounts().then(function(accounts) {
+            // first select an account to send transaction from
+            // no ether is sent, only gas is spent to make a tx
+            var acc = accounts[0];
+            return contract.methods.deposit(amt).send({from: acc});
+        }).then(function(tx) {
+            console.log(tx);
+        }).catch(function(tx) {
+            console.log(tx);
+        })
+    })
+
+    $('#withdraw').click(function() {
+        var amt = 0;
+        amt = parseInt($('#amount').val());
+
+        web3.eth.getAccounts().then(function(accounts) {
+            // first select an account to send transaction from
+            // no ether is sent, only gas is spent to make a tx
+            var acc = accounts[0];
+            return contract.methods.withdraw(amt).send({from: acc});
+        }).then(function(tx) {
+            console.log(tx);
+        }).catch(function(tx) {
+            console.log(tx);
+        })
     })
 })
